e9315bf1c0c790105a8c7d01e999ae77bc375060
Abdullah Farouk <31665944+navysealtf9k@users.noreply.github.com>
2018-05-06 17:09:28 -0400

    Added question 2b from assignment 8

--- Assignment 8.R
+++ Assignment 8.R
@@ -1,4 +1,8 @@
 ## Assignment 8 Problem 2b)
+#Suppose we want to generate random numbers from standard normal distribution whose density is given by φ(x) = (2π)−1/2 exp(−x2/2). We decide to use rejective sampling plan via double exponential distri- bution whose density function is given by: f0(x) = 1 exp(−|x|). 2
+
+#Write a code in R to implement the rejective sampling method to generate n = 1000 observations from N(0, 1).
+
 #install.packages('smoothmest')
 library(smoothmest)
 
@@ -19,47 +23,4 @@ while(sample_draw < N){
   }
 }
 qqnorm(accepted_samples)
-
-#Question 4a
-#Initial values
-x <- c(1.1777518, -0.5867896, 0.2283789, -0.1735369, -0.2328192,
-       1.0955114, 1.2053680, -0.7216797, -0.3387580, 0.1620835,
-       1.4173256, 0.0240219, -0.6647623, 0.6214567, 0.7466441,
-       1.9525066, -1.2017093, 1.9736293, -0.1168171, 0.4511754)
-obs <- length(x)
-
-
-#values
-sum_x <- sum(x)
-sum_x_2 <- sum(x^2)
-sum_x # found to be 7.018981
-sum_x_2 # found to be 17.94659
-
-
-#Question 4b
-
-#Gibbs sampling.
-#install.packages('invgamma')
-library(invgamma)
-
-
-#sampling from the posterior
-u <- rep(NA, 1001)
-variance <- rep(NA, 1001)
-burn <- 1
-variance[1] <- 2
- for(i in 2:10001){
-   u[i] <- rnorm(1, 4 * obs * mean(x) / (4*obs + variance[i - 1] ), sd =
-                     + sqrt(4 * variance[i-1] / (4*obs + variance[i-1])))
-   variance[i] <- rinvgamma(1, obs/2 + 5, 0.5*sum((x - u[i])^2) + 1)
- }
-
-#Removing burin
- u <- u[-burn]
-variance <- variance[-burn]
-
-#compute means
-mean(u) # computed to be 0.3510487
-mean(variance) # computed to be 0.6493934
-
 
